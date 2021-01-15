f8a682a0e44a3319df6eeef63fdb3024c39153f1
Tung Le Thanh <tunglt@vnpt.vn>
2021-01-15 09:22:35 +0700

    admin api

---
+++ AdminAPI/src/test/java/family/document/MainApplicationTest.java
@@ -0,0 +1,13 @@
+package family.document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
