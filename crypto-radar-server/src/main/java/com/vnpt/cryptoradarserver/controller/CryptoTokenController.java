2ea5b3ebfe585cd383e3b1f9a6e3db67386c40f0
Tung <letungcntt1@gmail.com>
2021-05-24 17:31:18 +0700

    df

--- crypto-radar-server/src/main/java/com/vnpt/cryptoradarserver/controller/CryptoTokenController.java
+++ crypto-radar-server/src/main/java/com/vnpt/cryptoradarserver/controller/CryptoTokenController.java
@@ -4,16 +4,22 @@ import com.vnpt.cryptoradarserver.model.CryptoToken;
 import com.vnpt.cryptoradarserver.repository.CryptoTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping(path = "cryptoController")
+@RequestMapping(path = "cryptoTokenController")
 public class CryptoTokenController {
 
     @Autowired
     private CryptoTokenRepository cryptoTokenRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody
+    String addNewUser(@Valid @RequestBody CryptoToken cryptoToken) {
+        cryptoTokenRepository.save(cryptoToken);
+        return "200";
+    }
 
     @GetMapping(path = "/all")
     public @ResponseBody
--- crypto-radar-server/src/main/java/com/vnpt/cryptoradarserver/model/CryptoToken.java
+++ crypto-radar-server/src/main/java/com/vnpt/cryptoradarserver/model/CryptoToken.java
@@ -1,5 +1,8 @@
 package com.vnpt.cryptoradarserver.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -8,14 +11,24 @@ public class CryptoToken {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    @Getter
+    @Setter
     private Long id;
 
+    @Getter
+    @Setter
     private String name;
 
+    @Getter
+    @Setter
     @Column(columnDefinition = "LONGTEXT")
     private String description;
 
+    @Getter
+    @Setter
     private Long createdBy;
 
+    @Getter
+    @Setter
     private Date createdTime;
 }
