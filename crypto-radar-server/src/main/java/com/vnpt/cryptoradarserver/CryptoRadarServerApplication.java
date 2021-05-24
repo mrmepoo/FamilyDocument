539e5824c480067a928df6ded31985c2c5cbc676
Tung <letungcntt1@gmail.com>
2021-05-24 15:01:09 +0700

    push crypto radar

---
+++ crypto-radar-server/src/main/java/com/vnpt/cryptoradarserver/CryptoRadarServerApplication.java
@@ -0,0 +1,13 @@
+package com.vnpt.cryptoradarserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRadarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoRadarServerApplication.class, args);
+	}
+
+}
---
+++ crypto-radar/src/main/java/com/vnpt/cryptoradar/CryptoRadarApplication.java
@@ -0,0 +1,13 @@
+package com.vnpt.cryptoradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoRadarApplication.class, args);
+	}
+
+}
